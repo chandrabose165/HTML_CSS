f420726a922dac51905ada5135d02fe7b0bd8ad7
chandrabose165 <varisibunny@gmail.com>
2026-08-28 20:06:31 +0530

    javascript

---
+++ JS/index.js
@@ -0,0 +1,28 @@
+console.log("hello");
+//console.log("i like pizza");
+ //window.alert("This is alert");
+ /*
+ This is  comment in java script
+  */
+ // comment
+
+ /*document.getElementById("myH1").textContent = 'hello';
+ document.getElementById("myp").textContent = "lorem";*/
+
+/* let age= 25;
+let price =10.99;
+let gpa =2.1;
+// console.log('you are {age} years old');
+*/
+let Fname = 'Chandrabose';
+let Age = 20;
+let isStudent =true;
+document.getElementById("p1").textContent = Fname;
+document.getElementById("p2").textContent = Age;
+document.getElementById("p3").textContent = isStudent;
+if(age >=20){
+    console.log("Adult");
+}
+else{
+    console.log("child");
+}
